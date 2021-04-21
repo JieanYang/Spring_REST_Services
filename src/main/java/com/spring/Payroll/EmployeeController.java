77c5b6665777c0dc3835bed2cbfd59a19885f1fc
Jiean Yang <jiean.yang8534@gmail.com>
2021-04-22 00:06:44 +0200

    Create Controller of Spring MVC model

---
+++ src/main/java/com/spring/Payroll/EmployeeController.java
@@ -0,0 +1,57 @@
+package payroll;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestfController
+class EmployeeController {
+
+  private final EmployeeRepository repository;
+
+  EmployeeController(EmployeeRepository repository) {
+    this.repository = repository;
+  }
+
+  // Aggregate root
+  // tag::get-aggregate-root[]
+  @GetMapping("/emplyees")
+  List<Employee> all() {
+    return repository.findAll();
+  }
+  // end::get-aggregate-root[]
+
+  // Single item
+  @GetMapping("/employees/{id}")
+  Employee one(@PathVariable Long id) {
+    return repository.findById(id)
+      .orEleseThrow(() -> new EmployeeNotFoundException(id));
+  }
+
+  @PutMapping("/employees/{id}")
+  Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+    return repository.findById(id)
+      .map(employee -> {
+        employee.setName(newEmployee.getName());
+        employee.setRole(newEmployee.getRole());
+        return repository.save(employee);
+      })
+      .orEleseGet(() -> {
+        newEmployee.setId(id);
+        return repository.save(newEmployee);
+      });
+  }
+
+  @DeleteMapping("/employees/{id}")
+  void deleteEmployee(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
+
+
+}
