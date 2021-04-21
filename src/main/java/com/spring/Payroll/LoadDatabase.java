661190fb9ba716a1c253d2840b5941d7e9621f75
Jiean Yang <jiean.yang8534@gmail.com>
2021-04-21 23:48:06 +0200

    Debug loadDatabase file

--- src/main/java/com/spring/Payroll/LoadDatabase.java
+++ src/main/java/com/spring/Payroll/LoadDatabase.java
@@ -4,18 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuaration;
+import org.springframework.context.annotation.Configuration;
 
-@Configuaration
+@Configuration
 class LoadDatabase {
+
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
   @Bean
-  CommandLineRunner initaDatabase(EmployeeRepository repository) {
+  CommandLineRunner initDatabase(EmployeeRepository repository) {
+    // @BUG ? : no show in console
     return args -> {
-      log.info("Preloading " + repository.save(new Employee("Bilbo Baggings", "burglar")));
+      log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
       log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
     };
   }
-
 }
