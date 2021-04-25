6a5cb763db8a5eb9a52d910e170f05ce5c9d5e36
Jiean Yang <jiean.yang8534@gmail.com>
2021-04-25 20:25:49 +0200

    What makes something RESTful ?

--- src/main/java/com/spring/Payroll/EmployeeController.java
+++ src/main/java/com/spring/Payroll/EmployeeController.java
@@ -1,6 +1,7 @@
 package payroll;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
 
 
 @RestController
@@ -25,12 +27,20 @@ class EmployeeController {
   // Aggregate root
   // tag::get-aggregate-root[]
   @GetMapping("/employees")
-  List<Employee> all() {
-    return repository.findAll();
+  CollectionModel<EntityModel<Employee>> all() {
+    List<EntityModel<Employee>> employees = repository.findAll().stream()
+      .map(employee -> EntityModel.of(employee, 
+        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(), 
+        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+      .collect(Collectors.toList());
+
+    return CollectionModel.of(employees, 
+      linkTo(methodOn(EmployeeController.class).all()).withSelfRel()
+    );
   }
   // end::get-aggregate-root[]
 
-  @PostMapping("/employees")
+  @PostMapping("/employees") 
   Employee newEmployee(@RequestBody Employee newEmployee) {
     return repository.save(newEmployee);
   }
