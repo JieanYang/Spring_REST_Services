a662d916d4f53345240938dee93e2a620fecd690
Jiean Yang <jiean.yang8534@gmail.com>
2021-04-29 00:00:50 +0200

    Creating a "complete" operation in the OrderController

--- src/main/java/com/spring/Payroll/OrderController.java
+++ src/main/java/com/spring/Payroll/OrderController.java
@@ -58,6 +58,30 @@ class OrderController {
 
   }
 
+
+  @PutMapping("/orders/{id}/complete")
+  ResponseEntity<?> complete(@PathVariable Long id) {
+    
+    Order order = orderRepository.findById(id)
+      .orElseThrow(() -> new OrderNotFoundException(id));
+    
+    if(order.getStatus() == Status.IN_PROGRESS) {
+      order.setStatus(Status.COMPLETED);
+      return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+    }
+
+    return ResponseEntity
+      .status(HttpStatus.METHOD_NOT_ALLOWED)
+      .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+      .body(
+        problem.create()
+        .withTitle("Method not allowed")
+        .withDetail("You can't complete an order that is in the " + order.getStatus() + " status")
+      );
+
+  }
+
+
   @DeleteMapping("/orders/{id}/cancel")
   ResponseEntity<?> cancel(@PathVariable Long id) {
 
@@ -76,8 +100,7 @@ class OrderController {
         Problem.created()
         .withTitle("Me6thod not allowed")
         .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status")
-      )
-
+      );
   }
 
 }
