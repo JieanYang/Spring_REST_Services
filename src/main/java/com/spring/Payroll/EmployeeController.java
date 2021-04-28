45d8d81a2e8f7bf278ddf2cfc6b9bd435a2af9ab
Jiean Yang <jiean.yang8534@gmail.com>
2021-04-28 23:38:49 +0200

    Create OrderController.java file

--- src/main/java/com/spring/Payroll/EmployeeController.java
+++ src/main/java/com/spring/Payroll/EmployeeController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.IanaLinkRelations;
 
